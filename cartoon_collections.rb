e4b7c161ef0c56bc077c28e665cfb59dd6f9479f
John Carter <johncarter352@gmail.com>
2020-04-04 07:53:52 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,7 +12,7 @@ def long_planeteer_calls(array)
   array.any? { |n| n.length > 4 }
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array)
+  array.include?(cheese_types)
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
