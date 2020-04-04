4b268d43422411012423de4254866c60ce57c54d
John Carter <johncarter352@gmail.com>
2020-04-04 07:58:26 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,5 +14,8 @@ end
 
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  array.include?(cheese_types)
+  if array.include?(cheese_types)
+    return cheese_type
+  else
+    false 
 end
